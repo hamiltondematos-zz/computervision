5f66f1b4aed3df2cfb9b7c62229c779f62056795
hamilton.matos <hamilton.matos@SPOD0434.sp01.local>
2016-02-29 14:51:59 -0300

    Ex 2.6

--- ComputerVision/src/computervision/chapter2/ex26/MainWindow26.java
+++ ComputerVision/src/computervision/chapter2/ex26/MainWindow26.java
@@ -156,7 +156,7 @@ public class MainWindow26 extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
